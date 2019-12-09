d0f8ff5951fca5565f488ca626066c6962d850b3
admin <18717429055@163.com>
2019-12-09 16:04:31 +0800

    企业管理

--- ace-cloud-vue/src/views/hqgj/BasicData/enterprise/Member.vue
+++ ace-cloud-vue/src/views/hqgj/BasicData/enterprise/Member.vue
@@ -22,6 +22,7 @@
                     <el-col :span="6">
                         <el-input
                                 @change="toggleChange"
+                                v-model="query.name"
                                 class="input-with-select"
                                 placeholder="请输入姓名或手机号码"
                                 style="float: right"
@@ -46,13 +47,13 @@
                 </el-table-column>
                 <el-table-column label="手机号码" prop="mobile">
                 </el-table-column>
-                <el-table-column label="添加时间" prop="createData" width="500">
+                <el-table-column label="添加时间" prop="createDate" width="500">
                 </el-table-column>
                 <el-table-column align="right" fixed="right" header-align="center" label="操作" width="60">
                     <template slot-scope="scope">
                         <!--<el-button @click="" height="40" type="text" @click="editPerson">编辑</el-button>-->
                         <!--<span class="strightline">|</span>-->
-                        <el-button @click="" type="text">删除</el-button>
+                        <el-button @click="Delete(scope.$index,scope.row)" type="text">删除</el-button>
                         <!--<span class="strightline">|</span>-->
                         <!--<el-button @click="previewPerson" type="text">详情</el-button>-->
                     </template>
@@ -73,6 +74,7 @@
 </template>
 
 <script>
+    import {personPage,deletePerson} from "@/api/hqgj/enterprise";
     export default {
         name: "Member",
         data() {
@@ -80,16 +82,65 @@
                 currentPage: 1, //初始页
                 pagesize: 10, //  每页的数据
                 total: 0,
-                rows:[
-                    {
-                        name:"王钦",
-                        mobile:"17688876666",
-                        createData: "2019-11-21 09:31:08"
-                    }
-                ]
+                rows:[],
+                query: {
+                    companyId:"",
+                    name: ""
+                },
             };
         },
+        created() {
+            this.getlist();
+            // this.AreaCodeQuery();
+        },
         methods:{
+            handleQuery: function () {
+                this.currentPage = 1;
+                this.getlist();
+            },
+            handleSizeChange: function (size) {
+                this.pagesize = size;
+                //每页下拉显示数据
+                this.getlist();
+            },
+            handleCurrentChange: function (currentPage) {
+                this.currentPage = currentPage;
+                //点击第几页
+                this.getlist();
+            },
+            getlist(){
+                this.query.companyId = this.$route.query.id;
+                this.query = Object.assign(this.query, {
+                    pageNum: this.currentPage,
+                    pageSize: this.pagesize,
+                    totalRecord: this.total
+                });
+                personPage(this.query).then(response => {
+                    this.total= response.total;
+                    this.rows = response.rows;
+                    console.log(response);
+                })
+            },
+            search(){
+                this.handleQuery();
+            },
+            Delete(index,data){
+                this.$confirm("此操作将永久删除该数据, 是否继续?", "提示", {
+                    confirmButtonText: "确定",
+                    cancelButtonText: "取消",
+                    type: "warning"
+                })
+                    .then(() => {
+                        this.id = data.id;
+                        deletePerson(this.id).then(response => {
+                            this.$message.success("删除成功");
+                            this.gelist();
+                        });
+                    })
+                    .catch(() => {
+
+                    });
+            },
             createPerson(){
                 this.$router.push({ path: "/hqgj/BasicData/enterprise/createPerson" });
             },
--- ace-cloud-vue/src/views/hqgj/BasicData/enterprise/details.vue
+++ ace-cloud-vue/src/views/hqgj/BasicData/enterprise/details.vue
@@ -4,40 +4,23 @@
         <div class="handle-box">
             <el-form :rules="rules" ref="ruleForm" label-width="600px" class="demo-ruleForm">
                 <el-form-item label="企业名称:" prop="name">
-                    <el-input style="width: 50%" placeholder="请输入企业名称"></el-input>
+                    <span>{{form.companyName}}</span>
                 </el-form-item>
                 <el-form-item label="统一社会信用代码:" prop="name">
-                    <el-input style="width: 50%" placeholder="请输入18位统一社会信用代码"></el-input>
+                    <span>{{form.creditCode}}</span>
                 </el-form-item>
                 <el-form-item label="法定代表人:" prop="name">
-                    <el-input style="width: 50%" placeholder="请输入法定代表人姓名"></el-input>
+                    <span>{{form.legalPerson}}</span>
                 </el-form-item>
                 <el-form-item label="联系方式:" prop="name">
-                    <el-input style="width: 21%" placeholder="联系人姓名"></el-input>
+                    <span>{{form.contactPersonName}}</span>
                     <span style="padding-left: 10px;padding-right: 10px">--</span>
-                    <el-input style="width: 26%" placeholder="联系电话"></el-input>
+                    <span>{{form.contactPersonTel}}</span>
                 </el-form-item>
                 <el-form-item label="地址:" prop="region">
-                    <el-select  placeholder="请选择省份" style="width: 12%;margin-right: 5px">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                    <el-select  placeholder="请选择市" style="width: 10%;margin-right: 5px">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                    <el-select  placeholder="请选择区县" style="width: 12%;margin-right: 5px">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                    <el-select  placeholder="请选择乡镇街道" style="width: 15%;margin-right: 5px">
-                        <el-option label="区域一" value="shanghai"></el-option>
-                        <el-option label="区域二" value="beijing"></el-option>
-                    </el-select>
-                    <el-input
-                            type="textarea"
-                            placeholder="请输入企业详细地址，xx社区xx路xx号(xx小区xx栋xx楼xx号）"
-                            style="width: 50%"></el-input>
+                    <span>{{form.areaCode}}</span>
+                    <br>
+                    <span>{{form.companyAddress}}</span>
                 </el-form-item>
                 <el-form-item>
                     <el-button @click="back">取消</el-button>
@@ -49,19 +32,27 @@
 </template>
 
 <script>
+    import {getById} from "@/api/hqgj/enterprise";
     export default {
         name: "edit",
         data() {
             return {
-                add: {
-                    type:1
-                }
+                form: {}
             };
         },
         created(){
-
+            this.getDetails();
         },
         methods:{
+            getDetails(){
+                this.id = this.$route.query.id;
+                getById(this.id)
+                    .then(response => {
+                        this.loading = false;
+                        this.form=response.data;
+                    })
+
+            },
             back(){
                 this.$router.push({ path: "/hqgj/BasicData/enterprise" });
             },
