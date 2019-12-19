0ccc6495a84cc5757b9aa7acc1a4aa56882538bb
heshuang <1335771162@qq.com>
2019-12-19 19:42:10 +0800

    区域转换

--- ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/BaseOrganizationVo.java
+++ ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/BaseOrganizationVo.java
@@ -5,4 +5,13 @@ import com.huacainfo.ace.hqgj.model.BaseOrganization;
 
 public class BaseOrganizationVo extends BaseOrganization {
 private static final long serialVersionUID = 1L;
+private String areaName;
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
 }
