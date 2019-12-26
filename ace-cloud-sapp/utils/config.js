e1b3326e36ca01d6a86c638ed4b52ba3a54c483f
songyang <734231879@qq.com>
2019-12-26 16:14:28 +0800

    创业服务包新增接口调试成功

--- ace-cloud-sapp/utils/config.js
+++ ace-cloud-sapp/utils/config.js
@@ -1,12 +1,14 @@
 var server = "http://192.168.2.114";
 var config = {
-    login: `${server}/hqgj-portal/security/login`,
+  login: `${server}/hqgj-portal/security/login`,
+  getByCategoryUrl: `${server}/hqgj-portal/dict/getByCategoryIds`,//字典
   trainPageUrl: `${server}/hqgj/www/serveCultivate/page`,  //培训列表page请求
   selectByIdDetailsUrl: `${server}/hqgj/www/serveCultivate/selectByIdDetails`,  //培训列表详情请求
   schedulePageUrl: `${server}/hqgj/www/serveCultivate/schedulePage`,  //日程请求列表
   scheduleGetByIdUrl: `${server}/hqgj/www/serveCultivate/scheduleGetById`,  //日程列表详情
   financePageUrL: `${server}/hqgj/www/serveFinance/page`,  //财税服务page请求
   financeIdUrl: `${server}/hqgj/www/serveFinance/getById`,  //财税服务列表详情
+  serveBusinessCreateUrl: `${server}/hqgj/serveBusiness/create`,  //新增创业服务
 };
 
 module.exports = config
