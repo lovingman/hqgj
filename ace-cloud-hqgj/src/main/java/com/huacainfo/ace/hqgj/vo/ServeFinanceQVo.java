2838232ee8c5e1c57173a7621591a7fcc549190e
heshuang <1335771162@qq.com>
2020-01-08 09:34:56 +0800

    微信小程序

--- ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/ServeFinanceQVo.java
+++ ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/ServeFinanceQVo.java
@@ -6,12 +6,20 @@ import com.huacainfo.ace.hqgj.model.ServeFinance;
 public class ServeFinanceQVo extends ServeFinance {
 private static final long serialVersionUID = 1L;
     private String[] types;
-
+    private String[] statuss;
     public String[] getTypes() {
         return types;
     }
 
     public void setTypes(String[] types) {
         this.types = types;
+    }
+
+    public String[] getStatuss() {
+        return statuss;
+    }
+
+    public void setStatuss(String[] statuss) {
+        this.statuss = statuss;
     }
 }
