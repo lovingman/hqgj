ab6bdbe07acd4bce2c6814211dee17ba897ea6c0
admin <18717429055@163.com>
2020-01-14 09:04:29 +0800

    优化

--- ace-cloud-vue/src/views/hqgj/ServicePack/BWSService/index.vue
+++ ace-cloud-vue/src/views/hqgj/ServicePack/BWSService/index.vue
@@ -75,7 +75,7 @@
       <el-table :data="list" class="table" max-height="475" ref="multipleTable" v-loading="loading">
         <el-table-column align="center" label="序号" type="index" width="55"></el-table-column>
         <el-table-column label="流水编号" prop="lsNo" width="180"></el-table-column>
-        <el-table-column label="注册企业名称" prop="companyName" :show-overflow-tooltip="true"  width="125"></el-table-column>
+        <el-table-column label="注册企业名称" prop="companyName" :show-overflow-tooltip="true"  min-width="125"></el-table-column>
         <el-table-column label="申请人" prop="applyPersonName" width="150"></el-table-column>
         <el-table-column label="联系电话" prop="applyPersonTel" width="180"></el-table-column>
         <el-table-column label="申请日期" prop="createDate" width="180"></el-table-column>
@@ -373,7 +373,7 @@ export default {
 
 .input-with-select {
   float: right;
-  width: 350px;
+  width: 300px;
 }
 
 .red {
