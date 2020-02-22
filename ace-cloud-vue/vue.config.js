9e353bf354904057053183357d915295c6efdcc8
douwenguo <18717429055@163.com>
2020-02-22 14:58:19 +0800

    代码

--- ace-cloud-vue/vue.config.js
+++ ace-cloud-vue/vue.config.js
@@ -1,8 +1,8 @@
 //vue.config.js
 const path = require('path')
+const webpack = require('webpack');
 module.exports = {
   //1.从vue cli3.3开始已弃用，请使用publicPath
-
   /*
     2.1部署应用包时的基本URL。用法和webpack本身的output.publicPath一致。
 但在cli的其它地方也会用到这个值，所以请不要直接修改webpack的output.publicPath。
@@ -24,7 +24,66 @@ module.exports = {
   devServer: {
     disableHostCheck: true
   },
+  lintOnSave: false,
+  runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
+  productionSourceMap: false, // 生产环境的 source map
+  /*configureWebpack: {
+    plugins: [
+      new webpack.DllReferencePlugin({
+        context: process.cwd(),
+        manifest: require("./public/vendor/vendor-manifest.json")
+      })
+    ]
+  },*/
   chainWebpack: config => {
+    config.plugin('prefetch').tap(options => {
+      options[0].fileBlacklist = options[0].fileBlacklist || []
+      options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
+      return options
+    });
+       /*
+    修改node_modules _lib-flexible@0.3.2@lib-flexible flexible.js
+    我们把代码改一下
+    function refreshRem(){
+        var width = docEl.getBoundingClientRect().width;
+        var rem = width / 10;
+        docEl.style.fontSize = "22" + 'px';
+        flexible.rem = win.rem = rem;
+    }
+    */
+			config.module
+			.rule('sass')
+			.oneOf('vue')
+			.use('px2rem-loader')
+			.loader('px2rem-loader')
+			.before('postcss-loader') //19.2 this makes it work.
+			.options({ remUnit:19.2, remPrecision: 8 })
+			.end()
+			config.module
+			.rule('css')
+			.oneOf('vue')
+			.use('px2rem-loader')
+			.loader('px2rem-loader')
+			.before('postcss-loader') // this makes it work.
+			.options({ remUnit:19.2, remPrecision: 8 })
+			.end()
+			  config.module
+				.rule('less')
+				.oneOf('vue')
+				.use('px2rem-loader')
+				.loader('px2rem-loader')
+				.before('postcss-loader') // this makes it work.
+				.options({ remUnit:19.2, remPrecision: 8 })
+				.end()
+			config.module
+			.rule('scss')
+			.oneOf('vue')
+			.use('px2rem-loader')
+			.loader('px2rem-loader')
+			.before('postcss-loader') // this makes it work.
+			.options({ remUnit:19.2, remPrecision: 8 })
+			.end()
+		console.log(config)
     const svgRule = config.module.rule('svg')
     // 清除已有的所有 loader。
     // 如果你不这样做，接下来的 loader 会附加在该规则现有的 loader 之后。
