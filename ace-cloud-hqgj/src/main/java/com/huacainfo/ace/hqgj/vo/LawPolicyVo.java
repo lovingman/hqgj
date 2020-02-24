669f5e18b34fa1f4166ac4f8a4096b58236c23b6
douwenguo <18717429055@163.com>
2020-02-24 14:47:50 +0800

    代码

--- ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/LawPolicyVo.java
+++ ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/vo/LawPolicyVo.java
@@ -1,8 +1,22 @@
 package com.huacainfo.ace.hqgj.vo;
 
+import com.huacainfo.ace.hqgj.model.BasicAnnex;
 import com.huacainfo.ace.hqgj.model.LawPolicy;
+
+import java.util.List;
 
 
 public class LawPolicyVo extends LawPolicy {
+    private List<BasicAnnex> basicAnnexes ;
 private static final long serialVersionUID = 1L;
+
+    @Override
+    public List<BasicAnnex> getBasicAnnexes() {
+        return basicAnnexes;
+    }
+
+    @Override
+    public void setBasicAnnexes(List<BasicAnnex> basicAnnexes) {
+        this.basicAnnexes = basicAnnexes;
+    }
 }
