0185e23a2630629eecddd838a1a08895e0584a8a
admin <18717429055@163.com>
2020-03-05 11:15:48 +0800

    优化

--- ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/model/ServeFinance.java
+++ ace-cloud-hqgj/src/main/java/com/huacainfo/ace/hqgj/model/ServeFinance.java
@@ -114,6 +114,7 @@ public class ServeFinance extends BaseModel {
      * 字段名：serve_finance.modifyDate
      * 注释：修改时间
      */
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date modifyDate;
 
     /**
